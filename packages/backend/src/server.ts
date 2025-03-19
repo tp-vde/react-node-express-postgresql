0e0277ae6e3a9f8bbdafffd4ef369393dd65955b
Vianney <vianney_mb@yahoo.fr>
2025-03-19 13:02:09 +0100

    rebase develop

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,21 +1,13 @@
-import MigrationManager from './utils/MigrationManager';
+import { MigrationManager } from "./utils/MigrationManager";
 import express from 'express';
 import { createRouter } from './routes/router';
-import { ConfigReader } from "./config/ConfigReader";
-import { fileConfig } from "./config/paths";
-import { readHttpServerOptions } from "./config/hostConfig";
-import { loggerService } from "./types/types";
-import bodyParser from 'body-parser';
+import logger from '../src/utils/logger';
 
-
-const config = new ConfigReader(fileConfig());
-const { listen: { port: listenPort } } = readHttpServerOptions(config.getConfig('backend'));
-
+const PORT = process.env.BACKEND_PORT || 7007;
 const databaseName = 'vde_database';
-const logger = loggerService();
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api', createRouter({logger}));
+app.use('/api', createRouter());
 
 async function startServer() {
   const migrationManager = new MigrationManager({ dbName: databaseName}); 
@@ -25,9 +17,9 @@ async function startServer() {
     await migrationManager.migrate();
     await migrationManager.close();
 
-    app.listen(listenPort, (err: any) => {
+    app.listen(PORT, (err: any) => {
       if(err) throw new Error(`Erreur :: ${err}` );
-      console.log(`Serveur démarré sur le port ${listenPort}`);
+      logger.info(`Serveur démarré sur le port ${PORT}`);
     });
 
   } catch (error) {
